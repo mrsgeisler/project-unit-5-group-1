1d69acabe579f24b454db9f0ae3ed672170b86f5
mrsgeisler <98759532+mrsgeisler@users.noreply.github.com>
2022-02-18 08:08:34 -0600

    Added monkey to the middle

--- main.ts
+++ main.ts
@@ -154,3 +154,22 @@ mySprite = sprites.create(img`
     . . . f f . . f f . . f f . . . 
     `, SpriteKind.Player)
 mySprite.setPosition(51, 85)
+mySprite = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f f . . . . 
+    . c d d d d d d e e d d f . . . 
+    . c d f d d f d e e b d c . . . 
+    c d d f d d f d e e b d c . f f 
+    c d e e d d d d e e f c . f e f 
+    c d d d d c d e e e f . . f e f 
+    . f c c c d e e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f f . 
+    . . f f e f e e f e e e e f . . 
+    . f e f f e e f f f e e e f . . 
+    f d d b d d c f f f f f f b f . 
+    f d d c d d d f . . f c d d f . 
+    . f f f f f f f . . . f f f . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(76, 85)
